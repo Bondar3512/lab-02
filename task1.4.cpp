8431d280ddd0a98f60ac5a9227980c3fd933c29b
Bondar3512 <90658768+Bondar3512@users.noreply.github.com>
2021-10-12 10:28:03 +0900

    Update task1.4.cpp

--- task1.4.cpp
+++ task1.4.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 using namespace std;
 int main(){
-    double x, y,eps=0-9;
+    double x, y,eps=1e-9;
 cout << "Coordinates x,y:\n";
     cin >> x >> y;
 if ((y<=-x)&&(x<=y)&&(y=x*x-2)) cout << "YES\n";
